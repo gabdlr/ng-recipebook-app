6e3e956a9a6b67e1fa51f14f925b79fa46777ab3
Gabriel De Los Rios <gab.delosrios@gmail.com>
2024-01-28 22:28:35 -0300

    test: unit tests for auth service

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -1,16 +1,37 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from "@angular/core/testing";
 
-import { AuthService } from './auth.service';
+import { AuthService } from "./auth.service";
+import { Store } from "@ngrx/store";
+import * as AuthActions from "./auth/store/auth.actions";
 
-describe('AuthService', () => {
+describe("AuthService", () => {
   let service: AuthService;
-
+  let storeSpy: jasmine.SpyObj<Store>;
+  const MOCK_EXPIRATION_DURATION = 1000;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    storeSpy = jasmine.createSpyObj(Store, ["dispatch"]);
+    TestBed.configureTestingModule({
+      providers: [{ provide: Store, useValue: storeSpy }],
+    });
     service = TestBed.inject(AuthService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should set expiration timer and call dispatch when time is up", fakeAsync(() => {
+    service.setLogoutTimer(MOCK_EXPIRATION_DURATION);
+    expect(service["tokenExpirationTimer"]).toBeTruthy();
+    tick(MOCK_EXPIRATION_DURATION);
+    expect(storeSpy.dispatch).toHaveBeenCalledOnceWith(
+      new AuthActions.Logout()
+    );
+  }));
+
+  it("should clear the timer if set", () => {
+    service.setLogoutTimer(MOCK_EXPIRATION_DURATION);
+    service.clearLogoutTimer();
+    expect(service["tokenExpirationTimer"]).toBe(null);
   });
 });
